b76bd7050fad0ea78fc0fde145cccbc0a717ac80
sevresbabylone <cheryl.francais@gmail.com>
2018-07-20 09:17:48 +0800

    adds exercise 2.1.4

---
+++ binary-addition.js
@@ -0,0 +1,20 @@
+function addition (a, b) {
+  var c = []
+  var carry = 0
+  for (var i = a.length - 1; i >= 0; i--) {
+    var sum = a[i] + b[i] + carry
+    c[i + 1] = (a[i] + b[i] + carry) % 2
+    if (sum >= 2) {
+      carry = 1
+    } else {
+      carry = 0
+    }
+  }
+  c[0] = carry
+  return c
+}
+
+var A = [0, 0, 1, 1]
+var B = [1, 0, 1, 1]
+console.log(addition(A, B))
+// C => [0, 1, 1, 1, 0]
