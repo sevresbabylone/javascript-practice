a25c4bfa945914ef085773970c6e34263834b3be
sevresbabylone <cheryl.francais@gmail.com>
2018-07-23 22:02:18 +0800

    adds notes

---
+++ mergeSort.js
@@ -0,0 +1,6 @@
+// Input: Unsorted Array of integers A
+// Output: Sorted Array of integers A
+
+// Loop invariant of Loop 3
+// At the start of each iteration of the loop,
+// subarray A[p...k-1] contains k - p smallest elements of L[] and R[] in sorted order
