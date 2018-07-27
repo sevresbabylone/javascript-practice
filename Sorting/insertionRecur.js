ea3b68fe0694a7d97bc7e3f93608a460f8052bbb
sevresbabylone <cheryl.francais@gmail.com>
2018-07-27 22:29:58 +0800

    adds insertion recursive

--- Sorting/insertionRecur.js
+++ Sorting/insertionRecur.js
@@ -3,7 +3,17 @@
 // A[n]􏰀 into the sorted array A[n - 1]􏰀.
 // Write a recurrence for the running time of this recursive version of insertion sort.
 
-
-function insertionRecur() {
-  
+function insertionRecur (A) {
+  if (A.length === 1) return A // base case
+  var smallerArray = insertionRecur(A.slice(0, A.length - 1))
+  var key = A[A.length - 1]
+  var i = smallerArray.length - 1
+  while (key < smallerArray[i] && i >= 0) {
+    smallerArray[i + 1] = smallerArray[i]
+    i = i - 1
+  }
+  smallerArray[i + 1] = key
+  return smallerArray
 }
+var unsortedArray = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
+console.log(insertionRecur(unsortedArray))
