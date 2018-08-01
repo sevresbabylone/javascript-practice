10648b487037365f6aa1a614caceae382c0f2d25
sevresbabylone <cheryl.francais@gmail.com>
2018-08-02 01:24:11 +0800

    updates

--- CLRS Exercises/binarySearchRecur.js
+++ CLRS Exercises/binarySearchRecur.js
@@ -5,7 +5,6 @@
 // remaining portion of the sequence each time.
 
 // Write pseudocode, either iterative or recursive, for binary search.
-// Argue that the worst-case running time of binary search is Θ(lg n).
 
 // Iterative
 // Input: Array of integers A, Integer Key
@@ -13,8 +12,8 @@
 
 function binarySearch (A, left, right, key) {
   if (right >= left) {
-    var midpoint = Math.floor(left + (right - 1) / 2)
-    if (A[midpoint === key]) {
+    var midpoint = Math.floor(left + (right - left) / 2)
+    if (A[midpoint] === key) {
       return midpoint
     }
     if (A[midpoint] > key) return binarySearch(A, left, midpoint - 1, key)
@@ -25,3 +24,8 @@ function binarySearch (A, left, right, key) {
 
 var oddNumbers = [1, 3, 7, 9, 11, 13, 15, 17, 21, 23, 25, 27]
 console.log('Element 13 is present at position ' + binarySearch(oddNumbers, 0, oddNumbers.length, 13))
+
+// Argue that the worst-case running time of binary search is Θ(lg n).
+// Worst case: Element is located at the very last mid-point or not present at all
+// 2 ^ N = n total number of elements, so N tries to get to last mid-point
+// therefore log 2 n = N is lgn
