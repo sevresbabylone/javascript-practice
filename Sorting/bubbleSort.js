06b49d87d3cf782fc58095e55f8703c7cea4eaa3
sevresbabylone <cheryl.francais@gmail.com>
2018-08-03 10:01:14 +0200

    adds bubblesort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort (A) {
+  for (var i = 0; i < A.length; i++) {
+    for (var j = A.length - 1; j > i + 1; j--) {
+      if (A[j] < A[j - 1]) {
+        var temp = A[j - 1]
+        A[j - 1] = A[j]
+        A[j] = temp
+      }
+      console.log(A)
+    }
+  }
+}
+
+var unsortedArray = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
+bubbleSort(unsortedArray)
+console.log(unsortedArray)
