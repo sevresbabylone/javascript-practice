bba210c22c230ec4acf80f2cc68f25f74faa4125
sevresbabylone <cheryl.francais@gmail.com>
2018-08-08 21:59:07 +0200

    adds non recur

---
+++ CLRS Exercises/binarySearch.js
@@ -0,0 +1,5 @@
+function binarySearch () {
+
+}
+var oddNumbers = [1, 3, 7, 9, 11, 13, 15, 17, 21, 23, 25, 27]
+console.log('Element 13 is present at position ' + binarySearch(oddNumbers, 0, oddNumbers.length, 13))
